14e636ac8e8bcc189b28c8b90b2e2815d304bc0b
Goh Jia Xin <135586980+GohJiaXin@users.noreply.github.com>
2025-12-04 15:58:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ class ChatResponse(BaseModel):
 app = FastAPI()
 
 # Initialize Cohere client once
-co = cohere.ClientV2(api_key=os.getenv("API_KEY"))
+co = cohere.ClientV2(api_key=os.getenv("rfsM0XhnKGtolqxREqTYfcJ6FOGwBcWxlPpljPPR"))
 
 @app.get("/math")
 def health():
